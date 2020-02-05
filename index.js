1eb58566250d5158a42a2757bb07b9142e33f47d
Sambego <sambellen@gmail.com>
2020-02-04 16:04:57 -1000

    Add JOSE slides

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 const boxen = require("boxen");
 const chalk = require("chalk");
 
@@ -8,29 +9,30 @@ const c = chalk.cyan;
 const b = chalk.bold;
 
 const cardOptions = {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'double',
-  borderColor: 'blue'
+    padding: 1,
+    margin: 1,
+    borderStyle: "double",
+    borderColor: "blue"
 };
 
-const content = `                  ${b('Sam Bellen')}
+const content = `                  ${b("Sam Bellen")}
 
-${g('Job')}                Developer Advocate Engineer at Auth0
-${g('Twitter')}            ${y('https://twitter.com/sambego')}
-${g('GitHub')}             ${y('https://github.com/sambego')}
-${g('Website')}            ${y('https://sambego.tech')}
-${g('LinkedIn')}           ${y('https://linkedin.com/in/sambellen/')}
+${g("Job")}                Developer Advocate Engineer at Auth0
+${g("Twitter")}            ${y("https://twitter.com/sambego")}
+${g("GitHub")}             ${y("https://github.com/sambego")}
+${g("Website")}            ${y("https://sambego.tech")}
+${g("LinkedIn")}           ${y("https://linkedin.com/in/sambellen/")}
 
-                    ${b('Slides')}
+                    ${b("Slides")}
 
-${g('JSON Web Tokens')}    ${y('https://jwt.sambego.tech')}
-${g('Passwordless')}       ${y('https://1990.sambego.tech')}
-${g('Webauthn')}           ${y('https://webauthn.sambego.tech')}
+${g("JSON Web Tokens")}    ${y("https://jwt.sambego.tech")}
+${g("Passwordless")}       ${y("https://1990.sambego.tech")}
+${g("Webauthn")}           ${y("https://webauthn.sambego.tech")}
+${g("JOSE")}               ${y("https://jose.sambego.tech")}
 
-${g('Web Audio')}          ${y('https://audio.sambego.tech')}
-${g('Web APIs')}           ${y('https://go-web.sambego.tech')}
-${g('Web APIs')}           ${y('https://meow.sambego.tech')}`;
+${g("Web Audio")}          ${y("https://audio.sambego.tech")}
+${g("Web APIs")}           ${y("https://go-web.sambego.tech")}
+${g("Web APIs")}           ${y("https://meow.sambego.tech")}`;
 
 const slidesContent = ``;
 

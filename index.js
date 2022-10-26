aeab77117c8a78f00824d8f247d35049165236b4
Sambego <sambellen@gmail.com>
2022-10-26 23:37:32 +0200

    Updates

--- index.js
+++ index.js
@@ -7,23 +7,25 @@ const y = chalk.yellow;
 const g = chalk.green;
 const c = chalk.cyan;
 const b = chalk.bold;
+const i = chalk.italic;
 
 const cardOptions = {
     padding: 1,
     margin: 1,
     borderStyle: "double",
-    borderColor: "blue"
+    borderColor: "blue",
+    title: `${b("Sam Bellen")} - ${i("Principal Developer Advocate at Auth0")}`,
+    titleAlignment: 'center'
 };
 
-const content = `                   ${b("Sam Bellen")}
+const content = `Social links
 
-${g("Job")}                Principal Developer Advocate at Auth0
 ${g("Twitter")}            ${y("https://twitter.com/sambego")}
 ${g("GitHub")}             ${y("https://github.com/sambego")}
 ${g("Website")}            ${y("https://sambego.tech")}
 ${g("LinkedIn")}           ${y("https://linkedin.com/in/sambellen/")}
 
-                   ${b("Slides")}
+${b("Slides")}
 
 ${g("JSON Web Tokens")}    ${y("https://jwt.sambego.tech")}
 ${g("Passwordless")}       ${y("https://1990.sambego.tech")}
